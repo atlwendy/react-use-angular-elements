7d9de6c11a5c1f8db31adb5d1072cd5689547ac9
Wendy Yang <atlwendy@gmail.com>
2019-02-28 13:31:46 -0500

    get angular elements shown in app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    let button = document.querySelector('ts-button');
+    let buttonHTMLElement = document.querySelector('.c-button');
+    let count = 0;
+    this.component.addEventListener('click', (event) => {
+      if (count % 2 === 0) {
+        buttonHTMLElement.textContent = "Click again!!";
+        button.format = 'fill';
+        button.theme = "primary";
+        button.collapsed = 'true';
+      } else {
+        buttonHTMLElement.textContent = "Click Me!!";
+        button.format = 'hollow';
+        button.theme = "warn";
+        button.collapsed = 'false';
+      }
+      count++;
+      if (count === 5) {
+        button.isDisabled = true;
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.component.removeEventListener('click', this.onClickSubmit);
+  }
+
+
+  handleRef = component => {
+    this.component = component;
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Here!!!
           </p>
           <a
             className="App-link"
@@ -20,6 +52,14 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div className="button">
+          <ts-button
+            buttonType="search"
+            theme="warn"
+            format="hollow"
+            ref={this.handleRef}
+          >Click Me!!</ts-button>
+        </div>
       </div>
     );
   }
